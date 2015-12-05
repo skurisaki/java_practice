e2d48e23e5601b8eebe4300e49df596bd65ac635
skurisaki <skurisaki@crooz.co.jp>
2015-12-05 20:36:16 +0900

    first

---
+++ practice4_9_4.java
@@ -0,0 +1,13 @@
+public class Main{
+	public static void main(String[] args){
+		int[] numbers = { 3, 4, 9 };
+		System.out.println("1Œ…‚Ì”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+
+	int input = new java.util.Scanner(System.in).nexiInt();
+
+	for(int n : numbers){
+		if(n == input){
+			System.out.println("ƒAƒ^ƒŠI");
+		}
+	}
+}
